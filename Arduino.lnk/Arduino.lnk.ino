24f546f42e4e17384aa10ce2d08da1eddb8da923
emilio <emoxic.sama@gmail.com>
2022-05-16 13:05:46 -0500

    Added arduino echo function

--- Arduino.lnk/Arduino.lnk.ino
+++ Arduino.lnk/Arduino.lnk.ino
@@ -4,12 +4,9 @@ int x, y, z, m, n, p;
 void(* resetFunc) (void) = 0;
 
 
-
-
 void setup() {
   Serial.begin(115200);
 }
-
 
 
 void loop() {
@@ -18,19 +15,14 @@ void loop() {
   //This line converts the input string into char
   char data_as_char[50];
   data.toCharArray(data_as_char, 50);
-  data = Serial.readString();
+  data = Serial.readStringUntil("/?");
 
-  
-  if (data.equals("reset")){
-    Serial.println("Resetting");
-    resetFunc();
-  }
-  else{
-  
+
     //This line converts char into a set of integers
-    if (sscanf(data_as_char , "%d/%d/%d/%d/%d/%d", &x, &y, &z, &m, &n, &p) == 6) {
+    if (sscanf(data_as_char , "#//%d/%d/%d/%d/%d/%d/", &x, &y, &z, &m, &n, &p) == 6) {
       Serial.println("Detected data: " + String(x) + ',' + String(y) + ',' + String(z) + ',' + String(m) + ',' + String(n) + ',' + String(p));
+    }else{
+      Serial.println("error");
     }
-  }
 
 }
